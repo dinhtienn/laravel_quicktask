ac5f9575865697e90609827b27cc6d45b189fc26
dinhtienn <dinhtiennguyen.1202@gmail.com>
2020-08-04 14:43:04 +0700

    Setup routes

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return view('tasks');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
